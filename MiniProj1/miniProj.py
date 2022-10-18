af1ecaa8262144518943cb64496060cbedd5749d
Neil Patel <neil.patel@plusgrade.com>
2022-10-18 00:40:20 -0400

    Update code

--- MiniProj1/miniProj.py
+++ MiniProj1/miniProj.py
@@ -1,7 +1,9 @@
+from __future__ import print_function
 from matplotlib import test
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.tree import DecisionTreeClassifier
 
 import json
 import matplotlib.pyplot as plt
@@ -34,19 +36,37 @@ def getVocabulary():
     X  = vectorizer.fit(comments)
     print("Vocabulary size: ", len(X.vocabulary_))
 
-def baseMNB(comments_train, comments_test, classification_train, classification_test, emotions_train, emotions_test):
-    vectorizer = CountVectorizer()
-    commentsTrainVector = vectorizer.fit_transform(comments_train)
-    commentsTestVector = vectorizer.fit_transform(comments_test)
-    emotionsTrainVector = vectorizer.fit_transform(emotions_train)
-    emotionsTestVector = vectorizer.fit_transform(emotions_test)
-
-    print(emotionsTrainVector)
+def baseMNB(commentsTrainVector, commentsTestVector, classification_train, classification_test, emotions_train, emotions_test):
 
     mnbClassifier = MultinomialNB()
-    mnbClassifier.fit(commentsTrainVector, emotions_train)
-    # print(mnbClassifier.predict(commentsTestVector))
+    
+    #emotions
+    mnbClassifier.fit(commentsTrainVector, emotions_train)    
+    emotions_pred = mnbClassifier.predict(commentsTestVector)
 
+    #classifications
+    mnbClassifier.fit(commentsTrainVector, classification_train)
+    classification_pred = mnbClassifier.predict(commentsTestVector)
+    
+    return emotions_pred, classification_pred
+
+def baseDT(commentsTrainVector, commentsTestVector, classification_train, classification_test, emotions_train, emotions_test):
+
+    dtClassifier = DecisionTreeClassifier()
+
+    #emotions
+    dtClassifier.fit(commentsTrainVector, emotions_train)
+    emotions_pred = dtClassifier.predict(commentsTestVector)
+    print(emotions_pred)
+
+    #classifications
+    dtClassifier.fit(commentsTrainVector, classification_train)
+    classification_pred = dtClassifier.predict(commentsTestVector)
+
+    # for x,y in zip(classification_pred, emotions_pred):
+    #     print(x + "\t\t" + y)
+
+    return emotions_pred, classification_pred
 
 
 def run_q1():
@@ -63,8 +83,15 @@ def run_q2(comments, classification, emotions):
     comments_train, comments_test, classification_train, classification_test, emotions_train, emotions_test = train_test_split(comments, classification, emotions, test_size=0.2, random_state=0)
     
     #2.3
-    baseMNB(comments_train, comments_test, classification_train, classification_test, emotions_train, emotions_test)
+    vectorizer = CountVectorizer()
+    commentsTrainVector = vectorizer.fit_transform(comments_train)
+    commentsTestVector = vectorizer.transform(comments_test)
 
+    #2.3.1
+    baseMNB(commentsTrainVector, commentsTestVector, classification_train, classification_test, emotions_train, emotions_test)
+
+    #2.3.2
+    baseDT(commentsTrainVector, commentsTestVector, classification_train, classification_test, emotions_train, emotions_test)
 
 for value in emotionsJSON:
     emotions.append(value[1])
